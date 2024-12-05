18a60980778f98ace3536fc3679c301e1ce39181
Andi T.A. <andi.andranto0@gmail.com>
2024-12-04 23:04:50 -0500

    ch 2.1 fini

--- ch02_1/exercice.py
+++ ch02_1/exercice.py
@@ -3,6 +3,13 @@
 
 def majuscule(mot):
     # TODO completer la fonction ici
+    nov_mot = ""
+    
+    for letter in mot:
+        letter = chr(ord(letter)-32)
+        nov_mot += letter
+    
+    mot = nov_mot
     return mot
 
 
@@ -20,3 +27,4 @@ if __name__ == '__main__':
         mots[i] = majuscule(mots[i])
 
     print(mots)
+
